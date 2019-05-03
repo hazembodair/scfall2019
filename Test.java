114e32cd855d4628aab75ab5559d6f8bc98efb9f
sharif <majdsharif@gmail.com>
2019-05-03 11:46:49 +0300

    I Added semicolon at Line 25

--- Test.java
+++ Test.java
@@ -22,4 +22,4 @@ public class Test
       System.out.println (" wish you Dr a nice day!");
       System.out.println(" Dunia Al-khalil ");   
 	  System.out.println ("Hello this is Ahmad Alawnah, my ID is 20170105");
-	  System.out.println("Sharif Sharif ")
+	  System.out.println("Sharif Sharif ");
